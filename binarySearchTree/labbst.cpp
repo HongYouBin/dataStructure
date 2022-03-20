5b83be3389e15112e5826b19fd6ddb8bee465839
HongYouBin <ghddbqls33@gmail.com>
2022-03-20 22:57:02 +0900

    binary search Tree

---
+++ binarySearchTree/labbst.cpp
@@ -0,0 +1,19 @@
+//B735539, È«À¯ºó
+#include "bst.h"
+
+int main()
+{
+    BST<string, double> bst;
+    string str;
+    double dval;
+
+    cout << "Enter string & double pairs:\n";
+
+    while (cin >> str >> dval)
+        bst.Insert(str, dval);
+    cout << endl << "Preorder traversal:   "; bst.Preorder();
+    cout << endl << "Inorder traversal:    "; bst.Inorder();
+    cout << endl << "Postorder traversal:  "; bst.Postorder();
+    cout << endl << "Levelorder traversal: "; bst.Levelorder();
+    cout << endl;
+}
